21870e36039b32a6c4d8a2ce997fa1b42a7ec640
andresvia <andres.via@gmail.com>
2017-01-11 16:45:22 -0500

    usando glob para encontrar el directorio de un repo barea

--- main.go
+++ main.go
@@ -5,34 +5,40 @@ import (
 	"github.com/phayes/hookserve/hookserve"
 	"github.com/urfave/cli"
 	"os"
+	"path/filepath"
 )
 
 var app *cli.App
 var listenValue int
 var secretValue string
+var rootValue string
 
 func init() {
 	app = cli.NewApp()
 	app.Usage = "Updates tuleap"
-
-	app.Flags = []cli.Flag{
-		cli.IntFlag{
-			Name:        "listen, l",
-			EnvVar:      "UPDATE_TULEAP_LISTEN",
-			Value:       8888,
-			Destination: &listenValue,
-		},
-		cli.StringFlag{
-			Name:        "secret, s",
-			EnvVar:      "UPDATE_TULEAP_SECRET",
-			Destination: &secretValue,
-		},
-	}
-
 	app.Commands = []cli.Command{
 		{
 			Name:   "run",
 			Action: runaction,
+			Flags: []cli.Flag{
+				cli.IntFlag{
+					Name:        "listen, l",
+					EnvVar:      "UPDATE_TULEAP_LISTEN",
+					Value:       8888,
+					Destination: &listenValue,
+				},
+				cli.StringFlag{
+					Name:        "secret, s",
+					EnvVar:      "UPDATE_TULEAP_SECRET",
+					Destination: &secretValue,
+				},
+				cli.StringFlag{
+					Name:        "root, r",
+					EnvVar:      "UPDATE_TULEAP_ROOT",
+					Value:       "/var/lib/tuleap/gitolite/repositories",
+					Destination: &rootValue,
+				},
+			},
 		},
 	}
 }
@@ -48,7 +54,15 @@ func runaction(ctx *cli.Context) (err error) {
 		select {
 		case event := <-server.Events:
 			if event.Type == "push" {
-				fmt.Println(event.Owner + " " + event.Repo + " " + event.Branch + " " + event.Commit)
+				var matches []string
+				var err error
+				if matches, err = filepath.Glob(rootValue + "/*/" + event.Repo + ".git"); err != nil {
+					fmt.Println(err.Error())
+					continue
+				}
+				for match := range matches {
+					fmt.Println(match)
+				}
 				// buscar en los directorios de tuleap event.Repo
 				// si no lo encontramos:
 				// fail
